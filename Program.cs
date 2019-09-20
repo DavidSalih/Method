539419f2f8a0f07fc5ee5c07b52e7c872601341e
Stealth <Stealth@MSI>
2019-09-19 21:27:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ Author: David Salih
+ Date: 9/19/2019
+ Description: Method
+ */
+
+using System;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            Console.Write("");
+
+            try
+            {
+                string usrinput = Console.ReadLine();
+
+                Console.WriteLine("Hello " + usrinput + "!");
+
+            }
+            catch
+            {
+                Console.WriteLine("Type name");
+                Console.WriteLine("ESC to exit");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
